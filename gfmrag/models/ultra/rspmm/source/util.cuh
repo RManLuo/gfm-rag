e81dec19da05a7d993f3c21c606a0e055d0e159d
Serge Panev <spanev@nvidia.com>
2025-09-10 01:15:43 -0700

    Add RSPMM kernel with fp32 accumulation

--- gfmrag/models/ultra/rspmm/source/util.cuh
+++ gfmrag/models/ultra/rspmm/source/util.cuh
@@ -55,6 +55,19 @@ __device__ c10::BFloat16 warp_reduce<c10::BFloat16>(c10::BFloat16 value) {
     return value;
 }
 
+// Specialization for float (explicit for mixed precision kernels)
+template <>
+__device__ float warp_reduce<float>(float value) {
+#pragma unroll
+    for (int delta = 1; delta < warpSize; delta *= 2)
+#if __CUDACC_VER_MAJOR__ >= 9
+        value += __shfl_down_sync(kFullMask, value, delta);
+#else
+        value += __shfl_down(value, delta);
+#endif
+    return value;
+}
+
 template<class scalar_t>
 __device__ scalar_t warp_broadcast(scalar_t value, int lane_id) {
 #if __CUDACC_VER_MAJOR__ >= 9
